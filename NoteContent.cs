070fb2dc95e9e4df4460820a5034f1d9667c06b8
jonahH <93222612+jonahh1@users.noreply.github.com>
2022-12-17 10:21:52 +0000

    commit

--- NoteContent.cs
+++ NoteContent.cs
@@ -21,11 +21,7 @@ class NoteContent
   {
     float buffer = 24;
     rect rec = new rect(NoteList.rec.w, 0, env.window.w-NoteList.rec.w, env.window.h);
-    if (selectedNoteIndex == -1)
-    {
-      Draw.TextAnchored("press the arrow beside a note to select it", rec.center, Anchor.middleCenter, 24, "2a2a2a");
-      return;
-    }
+    
     Note note = NoteManager.notes[selectedNoteIndex];
     Raylib.DrawRectangleGradientV(
       (int)(rec.x), 
@@ -33,8 +29,31 @@ class NoteContent
       (int)(rec.w), 
       (int)(buffer*4),Utils.HexToRGB(note.bannerCol), Utils.HexToRGB(note.bannerCol+"00")
     );
-    Draw.TextAnchored(note.title, rec.xy+buffer, Anchor.middleLeft, 32, "fff");
+    Draw.TextAnchored(note.title, rec.xy+buffer, Anchor.middleLeft, 32, env.theme.colors["background"]);
 
     //TODO: big, fully featured text editor, -_-
+    // text editor
+    
+      
+    // text renderer
+    int textSize = 18;
+    string content = note.content;
+    float x=0,y=0;
+    for (int i = 0; i < content.Length; i++)
+    {
+      if (content[i] == '\n')
+      {
+        x=0;
+        y++;
+      }
+      else if (content[i] == '\t') x+=4;
+      else
+      {
+        vec2 pos = new vec2(rec.x+buffer, rec.y+buffer*5);
+        Draw.Codepoint((int)content[i], new vec2(pos.x+x*textSize/2, pos.y+y*textSize), textSize, "fff");
+        x++;
+      }
+
+    }
   }
 }
